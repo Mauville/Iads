d70c88fb4f32adede7d62f97f7f1ee7dbc6b0fae
YOURUSERNAMEHERE <YOUREMAILHERE>
2019-05-02 12:51:04 -0500

    Primera parte de local st. con actividades

--- proto/js/activities.js
+++ proto/js/activities.js
@@ -20,6 +20,27 @@ $(document).ready(function () {
     });
 }); 
 
+if(localStorage.getItem('total-activities')==null){
+    localStorage.setItem('total-activities', 0);
+}
+
 function show_activities(){
+
+}
+
+function add_activity(activity1){
+    localStorage.setItem('total-activities', JSON.parse(localStorage.getItem('total-activities'))+1);
+
+    let log_user = JSON.parse(localStorage.getItem('logged_user'));
+    let knocki = JSON.parse(localStorage.getItem(log_user.knocki_used));
     
+    knocki.activities = knocki.activities + 1;
+
+    let activity = {
+        owner: knocki.owner,
+        place: knocki.place,
+        activity: activity1
+    }
+
+    localStorage.setItem('activity-'+ localStorage.getItem('total-activities'), JSON.stringify(activity));
 }
